76e7b0d739a93242e274868e788b3c4ce43beec4
lixinrui706 <xli21@mail.csuchico.edu>
2018-02-07 17:58:51 -0800

    the new version for letter

--- main.cpp
+++ main.cpp
@@ -11,27 +11,25 @@ int main()
   if(letter >= 65 && letter <= 90) 
   //when user's entry is between A-Z...
   {
-
-  cout<<" is an upper case letter!\n";
   cout<<letter;
+  cout<<" is an upper case letter!\n";
+  
 
   }
 
  else if(letter >= 97 && letter <= 122)
   //when user's entry is between a-z...
   {
-
-  cout<<" is a lower case letter!\n";
   cout<<letter;
+  cout<<" is a lower case letter!\n";
+
 
   }
   else if(letter >= 33 && letter < 65)
   //in all other cases...
    {
-
-   cout<<"?! Pssh. What are you talking about?\n";
    cout<<letter;
-
+   cout<<"?! Pssh. What are you talking about?\n";
    }
    if(letter >= 32 && letter <= 122)
   //no matter what they enter...
