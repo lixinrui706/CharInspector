017386930b32c37a9f4a2cdf0b18ddf1b958a39e
lixinrui706 <xli21@mail.csuchico.edu>
2018-02-07 17:51:47 -0800

    new version of letter

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
-//Author:
+//Author:Xinrui Li
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-
+  char letter;
   cout<<"What character do you want to know about?\n";
-
+  cin>>letter;
+  if(letter >= 65 && letter <= 90) 
   //when user's entry is between A-Z...
+  {
+
   cout<<" is an upper case letter!\n";
+  cout<<letter;
 
+  }
+
+ else if(letter >= 97 && letter <= 122)
   //when user's entry is between a-z...
+  {
+
   cout<<" is a lower case letter!\n";
+  cout<<letter;
 
+  }
+  else if(letter >= 33 && letter < 65)
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
+   {
 
+   cout<<"?! Pssh. What are you talking about?\n";
+   cout<<letter;
+
+   }
+   if(letter >= 32 && letter <= 122)
+  //no matter what they enter...
+   {
+
+   cout<<"The ASCII value is: ";
+   cout<<(int)letter<<endl;
+
+   }
   return 0;
 }
